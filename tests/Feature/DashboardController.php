1de77f5ddb9704af29fd93f489d93c173fe7455e
BakoL2323 <142969559+ardiyanto1234@users.noreply.github.com>
2025-04-28 15:18:20 +0700

    pesan

--- tests/Feature/DashboardController.php
+++ tests/Feature/DashboardController.php
@@ -11,7 +11,7 @@ class DashboardController extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+   /* public function test_example(): void
     {
         $response = $this->get('/');
 
